e52d1a19c53759c89682eae0ded5b594b2aca8d0
pasha <space@pashas-MacBook-Pro.local>
2015-08-18 10:59:43 +0530

    improved game

---
+++ helper.js
@@ -0,0 +1,6 @@
+left_bezier_1:null,
+    left_bezier_2:null,
+    left_bezier_1_control_1:null,
+    left_bezier_1_control_2:null,
+    left_bezier_2_control_1:null,
+    left_bezier_2_control_2:null,
